ed37c8998367fd1d2b1a0597353bbb11720a6dee
Sylar Z. Zhang <Sylar.Zhang@gov.bc.ca>
2022-07-08 08:11:02 -0700

    ServletRegistrationBean to accept biometrics and id instead of ws

--- jag-staffnet-biometrics-application/src/main/java/ca/bc/gov/open/staffnet/configuration/SoapConfig.java
+++ jag-staffnet-biometrics-application/src/main/java/ca/bc/gov/open/staffnet/configuration/SoapConfig.java
@@ -39,7 +39,7 @@ public class SoapConfig extends WsConfigurerAdapter {
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();
         servlet.setApplicationContext(applicationContext);
         servlet.setTransformWsdlLocations(true);
-        return new ServletRegistrationBean<>(servlet, "/ws/*");
+        return new ServletRegistrationBean<>(servlet, "/biometrics/*");
     }
 
     @Bean
